e94ef2572cfe4ad5db2e24c4a0569322a914e822
Andrea Adami <andrea.adami@gmail.com>
2018-12-02 02:38:28 +0100

    Replace u_int64_t with uint64_t from stdint.h
    Fix for:
    /xbmc/platform/linux/storage/UDisks2Provider.h:43:5:
     error: 'u_int64_t' does not name a type; did you mean 'uint64_t'?
    
    Signed-off-by: Andrea Adami <andrea.adami@gmail.com>
    
    %% original patch: 0004-Replace-u_int64_t-with-uint64_t-from-stdint.h.patch

--- xbmc/platform/linux/storage/UDisks2Provider.h
+++ xbmc/platform/linux/storage/UDisks2Provider.h
@@ -40,7 +40,7 @@ class CUDisks2Provider : public IStorageProvider
     std::string m_label;
     std::string m_device;
     bool m_isSystem = false;
-    u_int64_t m_size = 0;
+    uint64_t m_size = 0;
 
     explicit Block(const char *object);
     ~Block() = default;
