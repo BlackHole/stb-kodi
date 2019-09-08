3b4ec404fee9e389dcb6a35cfa9f27befa591100
Your Name <you@example.com>
2019-09-08 18:46:28 +0000

    try to include gstreamer-pbutils

--- cmake/modules/FindGstreamer.cmake
+++ cmake/modules/FindGstreamer.cmake
@@ -14,6 +14,7 @@ if(PKG_CONFIG_FOUND)
   pkg_check_modules(PC_GLIB glib-2.0 QUIET)
   pkg_check_modules(PC_GOBJECT gobject-2.0 QUIET)
   pkg_check_modules(PC_GSTREAMER gstreamer-1.0 QUIET)
+  pkg_check_modules(PC_GSTREAMERPBUTILS gstreamer-pbutils-1.0 QUIET)
 endif()
 
 find_path(GSTREAMER_INCLUDE_DIR NAMES gst/gst.h
@@ -24,13 +25,15 @@ find_library(GOBJECT_LIBRARY NAMES gobject-2.0
                          PATHS ${PC_GOBJECT_LIBDIR})
 find_library(GSTREAMER_LIBRARY NAMES gstreamer-1.0
                          PATHS ${PC_GSTREAMER_LIBDIR})
+find_library(GSTREAMERPBUTILS_LIBRARY NAMES gstreamer-pbutils-1.0
+                         PATHS ${PC_GSTREAMER_LIBDIR})
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(Gstreamer REQUIRED_VARS GSTREAMER_LIBRARY GSTREAMER_INCLUDE_DIR)
 
 if(GSTREAMER_FOUND)
-  set(GSTREAMER_LIBRARIES ${GSTREAMER_LIBRARY} ${GOBJECT_LIBRARY} ${GLIB_LIBRARY})
-  set(GSTREAMER_INCLUDE_DIRS ${GSTREAMER_INCLUDE_DIR})
+  set(GSTREAMER_LIBRARIES ${GSTREAMER_LIBRARY} ${GSTREAMERPBUTILS_LIBRARY} ${GOBJECT_LIBRARY} ${GLIB_LIBRARY})
+  set(GSTREAMER_INCLUDE_DIRS ${GSTREAMER_INCLUDE_DIR} ${GSTREAMERPBUTILS_INCLUDE_DIR})
   set(GSTREAMER_DEFINITIONS -DHAVE_LIBGSTREAMER=1)
 endif()
 
